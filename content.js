68db983e3aef0083c12fb670cd9918fb8c0b5c3c
Andrii Sydorchuk <inboxand11@gmail.com>
2025-08-27 20:59:42 +0300

    Add listed text setting

--- content.js
+++ content.js
@@ -2,27 +2,13 @@ window.addEventListener('load', () => {
     chrome.storage.local.get('listed', (result) => {
     const listed = result.listed;
 
-    if (!listed) {
-        console.log('no listed setted in options');
-        return;
-    } else {
-        console.log('listed loaded');
-    }
-
     setTimeout(() => {
         const editsBtns = Array.from(document.querySelectorAll('button')).filter(btn => {
             return Array.from(btn.querySelectorAll('span')).some(span => span.textContent.trim() === 'Edit');
         });
-    
-        if(editsBtns) {
-            console.log(editsBtns);
-        } else {
-            console.log('edit btns not selected');
-        }
 
         editsBtns.forEach(editBtn => {
             editBtn.addEventListener('click', (e) => {
-                console.log(e.target);
 
                 setTimeout(() => {
                     const addSpecificSpan = Array.from(document.querySelectorAll('button span')).find(span => span.textContent.trim() === 'Add custom specific');
@@ -30,7 +16,14 @@ window.addEventListener('load', () => {
                     const addSpecificBtn = addSpecificSpan?.closest('button');
 
                     addSpecificBtn.addEventListener('click', (e) => {
-                        console.log(e.target);
+                        
+                        setTimeout(() => {
+                            const nameInputs = document.querySelectorAll('input[placeholder="Name"]');
+                            const listedTextInput = nameInputs[nameInputs.length - 1];
+                            
+                            listedTextInput.value = 'LISTED';
+                            listedTextInput.dispatchEvent(new Event('input', { bubbles: true }));
+                        }, 500);
                     });
                 }, 3000);
 
