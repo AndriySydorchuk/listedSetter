9594460f21d4815d950844cdff4def8aa558ced1
Andrii Sydorchuk <inboxand11@gmail.com>
2025-08-27 20:28:54 +0300

    Add delayed selection of all edit buttons and click handler for each
    A 3-second delay eliminates problems with buttons not loading immediately

--- content.js
+++ content.js
@@ -9,7 +9,22 @@ window.addEventListener('load', () => {
         console.log('listed loaded');
     }
 
+    setTimeout(() => {
+        const editsBtns = Array.from(document.querySelectorAll('button')).filter(btn => {
+            return Array.from(btn.querySelectorAll('span')).some(span => span.textContent.trim() === 'Edit');
+        });
+    
+        if(editsBtns) {
+            console.log(editsBtns);
+        } else {
+            console.log('edit btns not selected');
+        }
 
-
+        editsBtns.forEach(editBtn => {
+            editBtn.addEventListener('click', (e) => {
+                console.log(e.target);
+            });
+        });
+    }, 3000);
   });
 });
