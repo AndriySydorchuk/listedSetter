f2ad51bb95fd8ad90e1da82c0df31ee3c9a69746
Andrii Sydorchuk <inboxand11@gmail.com>
2025-09-17 18:34:35 +0300

    Add input for brand name

--- options.js
+++ options.js
@@ -1,19 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const brandInput = document.getElementById('brandName');
   const listedInput = document.getElementById('listedNum');
   const saveBtn = document.getElementById('saveBtn');
   const status = document.getElementById('status');
 
-  chrome.storage.local.get('listed', (result) => {
+  chrome.storage.local.get(['brand', 'listed'], (result) => {
     if (result.listed) {
       listedInput.value = result.listed;
+    }
+    if(result.brand) {
+      brandInput.value = result.brand;
     }
   });
 
   saveBtn.addEventListener('click', () => {
+    const brand = brandInput.value.trim();
     const listed = listedInput.value.trim().toUpperCase();
 
-    chrome.storage.local.set({ listed: listed }, () => {
+    chrome.storage.local.set({ brand: brand, listed: listed }, () => {
       status.textContent = 'saved';
+      console.log(`brand saved ${brand}`);
       console.log(`listed saved ${listed}`);
       setTimeout(() => { status.textContent = ''; }, 1500);
     });
